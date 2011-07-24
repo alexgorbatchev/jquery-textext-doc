0b8cc78826436bc72df33b5ecb788b06ea76fb1a
Alex Gorbatchev <alex.gorbatchev@gmail.com>
2011-07-23 20:54:41 -0700

    Switched to Jekyll.

---
+++ source/_plugins/textext.rb
@@ -0,0 +1,22 @@
+module Jekyll
+  class TextExtGenerator < Generator
+    safe false
+    
+    def generate(site)
+    end
+  end
+
+  class Site
+    alias jekyll_process process
+    
+    def process
+      jekyll_process
+
+      from = File.expand_path(config['textext'])
+      to   = File.expand_path(config['destination'] + '/textext')
+      `ln -s \"#{from}\" \"#{to}\"`
+      puts "Created TextExt symlink"
+    end
+  end
+end
+

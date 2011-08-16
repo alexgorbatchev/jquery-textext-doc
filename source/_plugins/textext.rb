06e215326d18c2fc39de88c4c304cf4bd365ff8b
Alex Gorbatchev <alex.gorbatchev@gmail.com>
2011-08-16 23:11:29 +0200

    Added ability to pull in documentation from JS files using @id notation and custom {% doc %} tag.

--- source/_plugins/textext.rb
+++ source/_plugins/textext.rb
@@ -1,3 +1,5 @@
+require 'kramdown'
+
 module Jekyll
   class TextExtGenerator < Generator
     safe false
@@ -18,5 +20,54 @@ module Jekyll
       puts "Created TextExt symlink"
     end
   end
+
+  class DocTag < Liquid::Tag
+    def initialize(tag_name, args, tokens)
+      super
+
+      parts = args.strip.split(/, /)
+      @file = parts[0]
+      @selector = parts[1]
+      @selector_regex = /@id (#{Regexp.escape(@selector)})/
+    end
+
+    def render(context)
+      full_path = File.expand_path('../jquery-textext/src' + '/' + @file)
+      source = File.read(full_path)
+      match = source.match(@selector_regex)
+
+      return "`#{@selector}` not found in #{full_path}" unless match
+
+      pos       = match.pre_match.length
+      left_pos  = pos - match.pre_match.reverse.index('**/')
+      right_pos = pos + match.to_s.length + match.post_match.index('*/')
+      comments  = source[left_pos, right_pos - left_pos]
+
+      # clean up indents and `*` at the begining of each line
+      lines = comments.lines.entries
+      lines.reject! { |line| line == '/**' or line == '*/' }
+      lines.map! { |line| line.sub(/^\s*\*\s*/, '') }
+
+      comments = ''
+      line_index = 0
+
+      # grab all lines up until the first javadoc argument
+      lines.each do |line|
+        break if line =~ /^@\w+/
+        line_index += 1
+        line = "\n" if line.strip == ''
+        comments << line
+      end
+
+      # all remaining lines are javadoc params
+      params = lines[line_index, lines.length - line_index]
+
+      doc = Kramdown::Document.new(comments, :auto_ids => false, :footnote_nr => 1)
+      doc.to_html
+    end
+  end
+
 end
+
+Liquid::Template.register_tag('doc', Jekyll::DocTag)
 

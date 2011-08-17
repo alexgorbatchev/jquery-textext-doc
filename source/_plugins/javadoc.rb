993c2bddc7f7262ec63f474f6e5e81c406607e31
Alex Gorbatchev <alex.gorbatchev@gmail.com>
2011-08-17 22:06:11 +0200

    Updated doc_* tags to function with single doc_file tag to establish and cache the source file.

--- source/_plugins/javadoc.rb
+++ source/_plugins/javadoc.rb
@@ -4,30 +4,30 @@ module Jekyll
   class DocTag < Liquid::Tag
     def initialize(tag_name, args, tokens)
       super
+      init(args)
+    end
 
-      parts           = args.strip.split(/, /)
-      @file           = parts[0]
-      @selector       = parts[1]
-      @selector_regex = /@id (#{Regexp.escape(@selector)})/
+    def init(args)
+      @@file      = args.strip
+      # TODO use config to get the path
+      @@full_path = File.expand_path('../jquery-textext/src' + '/' + @@file)
+      @@source    = File.read(@@full_path)
     end
 
     def kramdown(source)
       Kramdown::Document.new(source, :auto_ids => false, :footnote_nr => 1).to_html
     end
 
-    def extract_comments_from_file(file_path, selector_regex)
-      # TODO use config to get the path
-      full_path = File.expand_path('../jquery-textext/src' + '/' + file_path)
-      source    = File.read(full_path)
-      match     = source.match(@selector_regex)
+    def extract_comments_from_file(selector_regex)
+      match = @@source.match(@selector_regex)
 
-      return "<p>`#{@selector}` not found in `#{full_path}`</p>" unless match
+      return kramdown("`#{@selector}` not found in `#{@@full_path}`") unless match
 
       pos       = match.pre_match.length
       left_pos  = pos - match.pre_match.reverse.index('**/')
       right_pos = pos + match.to_s.length + match.post_match.index('*/')
 
-      source[left_pos, right_pos - left_pos]
+      @@source[left_pos, right_pos - left_pos]
     end
 
     def get_tags(lines)
@@ -91,8 +91,8 @@ module Jekyll
       kramdown(description)
     end
 
-    def get_comments_block(file_path, selector_regex)
-      comments = extract_comments_from_file(file_path, selector_regex)
+    def get_comments_block
+      comments = extract_comments_from_file(@selector_regex)
 
       # clean up indents and `*` at the begining of each line
       lines = comments.lines.entries
@@ -104,16 +104,25 @@ module Jekyll
         :tags => get_tags(lines)
       }
     end
+  end
 
+  class DocBaseTag < DocTag
+    def init(args)
+      @selector       = args.strip
+      @selector_regex = /@id (#{Regexp.escape(@selector)})\s*$/
+    end
+  end
+
+  class DocDescriptionTag < DocBaseTag
     def render(context)
-      comments = get_comments_block(@file, @selector_regex)
+      comments = get_comments_block
       comments[:description]
     end
   end
 
-  class DocOptTag < DocTag
+  class DocOptTag < DocBaseTag
     def render(context)
-      comments = get_comments_block(@file, @selector_regex)
+      comments = get_comments_block
 
       "<div class=\"option\">" +
         "<h4>#{comments[:tags][:name]} <span class=\"default\">(#{comments[:tags][:default]})</span></h4>" +
@@ -122,9 +131,9 @@ module Jekyll
     end
   end
 
-  class DocEventTag < DocTag
+  class DocEventTag < DocBaseTag
     def render(context)
-      comments = get_comments_block(@file, @selector_regex)
+      comments = get_comments_block
 
       "<div class=\"event\">" +
         "<h4>#{comments[:tags][:name]}</h4>" +
@@ -133,9 +142,9 @@ module Jekyll
     end
   end
 
-  class DocMethodTag < DocTag
+  class DocMethodTag < DocBaseTag
     def render(context)
-      comments = get_comments_block(@file, @selector_regex)
+      comments = get_comments_block
 
       "<div class=\"method\">" +
         "<h4>#{comments[:tags][:signature]}</h4>" +
@@ -145,7 +154,8 @@ module Jekyll
   end
 end
 
-Liquid::Template.register_tag('doc'        , Jekyll::DocTag)
+Liquid::Template.register_tag('doc_file'   , Jekyll::DocTag)
+Liquid::Template.register_tag('doc_desc'   , Jekyll::DocDescriptionTag)
 Liquid::Template.register_tag('doc_opt'    , Jekyll::DocOptTag)
 Liquid::Template.register_tag('doc_event'  , Jekyll::DocEventTag)
 Liquid::Template.register_tag('doc_method' , Jekyll::DocMethodTag)

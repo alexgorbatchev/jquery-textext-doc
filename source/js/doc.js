4c6c0b34e3583b8576df4a9d0eb6728bc40619b0
Alex Gorbatchev <alex.gorbatchev@gmail.com>
2011-08-23 21:58:21 +0100

    Working on the UI.
    Added auto TOC generation and ability to roll over method arguments.

--- source/js/doc.js
+++ source/js/doc.js
@@ -1,6 +1,95 @@
 $(function()
 {
 
-	console.log($('h3, h4'));
+	function enhanceMethodSignature(a)
+	{
+		var html = a.html();
+
+		html = html.replace(/\((.+)\)\s*$/, function(match, args)
+		{
+			return '('
+				+ args.replace(/\w+/g, function(arg)
+				{
+					return '<span class="param">' + arg + '</span>';
+				})
+				+ ')'
+			;
+		});
+
+		a.html(html);
+
+		function getParam(a)
+		{
+			return $(a)
+				.parents('.method:first')
+					.find('.params .' + $(a).text())
+						.parents('li:first')
+			;
+		};
+
+		a.find('.param').hover(
+			function() { getParam(this).addClass('highlight') },
+			function() { getParam(this).removeClass('highlight') }
+		);
+	};
+
+	// add TOC to the API doc page
+	(function()
+	{
+		var blocks = [];
+
+		$('#api section h3').each(function()
+		{
+			var h3    = $(this),
+				links = h3.parent().find('h4 a'),
+				block
+				;
+
+			if(links.length == 0)
+				return;
+
+			links = links.map(function()
+			{
+				var a = $(this);
+				enhanceMethodSignature(a);
+				return '<li><a href="#' + a.attr('name') + '">' + a.html() + '</a></li>';
+			})
+			.toArray();
+
+			block = $('<div class="block"><h4>' + h3.html() + '</h4><ul>' + links.join('') + '</ul></div>');
+			blocks.push(block);
+			$('#toc').append(block);
+		});
+
+		$('#toc').append('<div style="clear: both"/>');
+
+		$.each(blocks, function()
+		{
+			this.css('width', Math.floor(100 / blocks.length) + '%');
+		});
+	})();
+
+	(function()
+	{
+		var params = /^([\w\.\…]+) (\{\w+\})?(.*)$/;
+
+		$('#api ul.params li > p').each(function()
+		{
+			var p     = $(this),
+				html  = p.html(),
+				match = html.match(params)
+				;
+
+			if(match)
+			{
+				p.html(
+					'<code class="var ' + match[1] + '">' + match[1] + '</code> '
+					+ (match[2] ? '<code class="type">' + match[2] + '</code> ' : '')
+					+ match[3]
+				);
+			}
+		});
+
+	})();
 
 });
